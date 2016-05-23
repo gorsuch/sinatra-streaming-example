d52aa267ccc7ad14721530c5ccedf8bf8b72cb88
Christopher Stolt <stolt45@gmail.com>
2016-05-23 15:50:35 -0700

    REDISTOGO_URL to REDIS_URL

--- worker.rb
+++ worker.rb
@@ -1,6 +1,6 @@
 require 'redis'
 
-redis_url = ENV["REDISTOGO_URL"] || "redis://localhost:6379"
+redis_url = ENV["REDIS_URL"] || "redis://localhost:6379"
 uri = URI.parse(redis_url)
 r = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
